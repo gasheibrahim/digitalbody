694465e5b17eeb59ebbcc66744f7c4bdb4d5715b
gasheibrahim <ibrahimgashema154@gmail.com>
2020-10-04 10:30:13 +0200

    login and sign up and finalize design

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   get 'about/index'
   resources :appointments
   resources :patiences
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+  }
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
